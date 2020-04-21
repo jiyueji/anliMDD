11c5827d6d232760c97fdc888cf787594e2cd2b6
zhangwei <zhangwei>
2020-04-21 19:06:42 +0800

    ip

--- client/src/services/ApiService.js
+++ client/src/services/ApiService.js
@@ -8,8 +8,8 @@ class ApiSerice {
 
 
     constructor() {
-       // this.api_url = 'http://52.82.35.187:5000'; //'http://52.82.35.187:5000'
-        this.api_url = 'http://localhost:80';
+       this.api_url = 'http://52.82.35.187:5000'; //'http://52.82.35.187:5000'
+        // this.api_url = 'http://localhost:80';
     }
 
     /**
